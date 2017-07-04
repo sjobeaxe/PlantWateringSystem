9a67cf9743c419d60e6252e3ffe85e400508dcc1
Axel Sjöberg <sjobeaxe@gmail.com>
2017-07-04 14:31:26 +0300

    First version

---
+++ main.c
@@ -0,0 +1,28 @@
+/*
+ * File:   main.c
+ * Author: sjobe
+ *
+ * Created on 3. heinäkuuta 2017, 21:08
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    int x[64];
+    
+    x[0] = 1;
+    
+    if (x[0] == 1) 
+    {
+        x[1] = 2;
+    }
+    
+    struct plant 
+    {
+        int plant_id;
+        int 
+    };
+    
+    return;
+}
