fb87d81179e4d35992abced228f7a7aa55028ac1
Anders <Anders@Anders-PC.elektrooppi.fi>
2017-07-04 18:58:24 +0300

    Deleted unnecessary build files.

--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=PlantWaterer.X
+PROJECTNAME=PlantWateringSystem
 
 # Active Configuration
 DEFAULTCONF=default
